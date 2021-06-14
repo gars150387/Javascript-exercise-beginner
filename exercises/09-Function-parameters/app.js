82cabba4f57e32bf91057557bea66a57ba09e1e3
gars150387 <gar.santeliz@gmail.com>
2021-06-14 15:23:15 +0000

    origin

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+function renderPerson (name,dob,color,age,gender){
+    return name+ ' is a '+age+ ' years old '+gender+' born in '+ dob +' with '+color+ ' eyes';
+}
 
 
   ///*** Do not edit below this line ****/
